2f7531b76d79024726776fcc7245c4bc7309de4d
Marvin Lucas Heinz <heinmarv@rbb.do.nw.schule.de>
2022-08-24 14:52:03 +0200

    Initial Commit

---
+++ Youth/YouthApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouthApp.swift
+//  Youth
+//
+//  Created by Marvin Heinz on 24.08.22.
+//
+
+import SwiftUI
+
+@main
+struct YouthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
